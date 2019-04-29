8e96ccd4b99d79ba58c7af077fca70796787386d
crestasom <crestasom@gmail.com>
2019-04-29 20:17:03 +0545

    client request mappping

--- src/main/java/com/crestasom/demo/HomeController.java
+++ src/main/java/com/crestasom/demo/HomeController.java
@@ -1,5 +1,8 @@
 package com.crestasom.demo;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,11 @@ public class HomeController {
 	
 	
 	@RequestMapping("home")
-	public String show() {
-		System.out.println("Hi");
+	public String show(HttpServletRequest request) {
+		String name=request.getParameter("name");
+		HttpSession session=request.getSession();
+		System.out.println("Hi"+name);
+		session.setAttribute("name", name);
 		return "home";
 	}
 
--- src/main/webapp/pages/home.jsp
+++ src/main/webapp/pages/home.jsp
@@ -1,3 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-Home Page
-</html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Spring app</title>
+
+
+
+</head>
+Hello ${name}
