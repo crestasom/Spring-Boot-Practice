3aa1bbe529af03b720fd681b39cf8b3740e5a555
crestasom <crestasom@gmail.com>
2019-04-29 20:05:21 +0545

    Project Setup Complete

---
+++ src/main/java/com/crestasom/demo/HomeController.java
@@ -0,0 +1,17 @@
+package com.crestasom.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	
+	@RequestMapping("home")
+	public String show() {
+		System.out.println("Hi");
+		return "home";
+	}
+
+}
